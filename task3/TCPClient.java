c28da39ab74eb389f442c1cce1b4b7c5cfd4f49a
EmilGoransson <emilgo@kth.se>
2023-02-16 14:42:35 +0100

    task 3 maybe done?

---
+++ task3/TCPClient.java
@@ -0,0 +1,94 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+
+public class TCPClient {
+    boolean shutdown = false;
+    Integer timeout = null;
+    Integer limit = null;
+    public TCPClient(boolean shutdown, Integer timeout, Integer limit) {
+        this.shutdown = shutdown;
+        this.timeout = timeout;
+        this.limit = limit;
+    }
+    public TCPClient() {
+    }
+
+    public byte[] askServer(String hostname, int port) throws IOException{
+        if(timeout == null){
+            timeout = 0;
+        }
+        if(limit == null){
+            limit = Integer.MAX_VALUE;
+        }
+        int countByte = 0;
+
+
+        ByteArrayOutputStream testBuffer = new ByteArrayOutputStream();
+        int data = 0;
+
+        Socket cSocket = new Socket(hostname, port);
+        //shutdown
+        if(shutdown){
+            cSocket.shutdownOutput();
+        }
+        cSocket.setSoTimeout(timeout);
+        try{
+            while(data != -1 && countByte < limit){
+                data = cSocket.getInputStream().read();
+
+                if(data != -1 && countByte < limit) {
+                    countByte++;
+                    testBuffer.write(data);
+                }
+            }
+            cSocket.close();
+            return testBuffer.toByteArray();
+        }
+        catch(SocketException e){
+            cSocket.close();
+            return testBuffer.toByteArray();
+        }
+
+
+    }
+
+
+    public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
+        ByteArrayOutputStream testBuffer = new ByteArrayOutputStream();
+        int data = 0;
+        if(timeout == null){
+            timeout = 0;
+        }
+        if(limit == null){
+            limit = Integer.MAX_VALUE;
+        }
+        int countByte = 0;
+
+        Socket cSocket = new Socket(hostname, port);
+        cSocket.getOutputStream().write(toServerBytes);
+        //shutdown
+        if(shutdown){
+            cSocket.shutdownOutput();
+        }
+        cSocket.setSoTimeout(timeout);
+        try{
+            while(data != -1 && countByte < limit){
+                data = cSocket.getInputStream().read();
+
+                if(data != -1 && countByte < limit) {
+                    countByte++;
+                    testBuffer.write(data);
+                }
+            }
+            cSocket.close();
+            return testBuffer.toByteArray();
+        }
+        catch(SocketTimeoutException e){
+            cSocket.close();
+            return testBuffer.toByteArray();
+        }
+    }
+}
